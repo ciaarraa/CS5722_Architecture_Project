4f082d2c3e34ea9e7f5121d7ca8729385ffa5c52
David O'Riordan <oriordandj@gmail.com>
2021-03-04 22:38:34 +0000

    NYPizzaStore added

--- src/main/java/com/Factory/NYPizzaStore.java
+++ src/main/java/com/Factory/NYPizzaStore.java
@@ -1,24 +1,60 @@
-/*package com.Factory;
+package com.Factory;
 
+import com.Builder.PizzaTreeBuilder;
 import com.Pizza.*;
 
 public class NYPizzaStore extends PizzaStore {
-
     @Override
+
     public PizzaComposite createPizza(String type) {
         switch (type) {
             case "cheese":
-                return new NYStyleCheesePizza();
+
+                PizzaTreeBuilder cheesePizza = new PizzaTreeBuilder("Tasty Cheesy");
+                cheesePizza.addIngredient("cheese", 200, 400);
+
+                return cheesePizza.getPizza();
+
             case "pepperoni":
-                return new NYStylePepperoniPizza();
+
+                PizzaComposite NYStylePepperoni = new PizzaComposite( "New York Pepperoni");
+                PizzaComposite PepperoniCheese = new PizzaComposite("Pepperoni Cheese");
+
+                PepperoniCheese.add(new PizzaIngredient("Mozzarella", 600, 2.50));
+                PepperoniCheese.add(new PizzaIngredient("Cheddar", 400, 1.50));
+
+                NYStylePepperoni.add(PepperoniCheese);
+                NYStylePepperoni.add(new PizzaIngredient("Pepperoni", 250, 2.50));
+
+                return NYStylePepperoni;
+
+
             case "clam":
-                return new NYStyleClamPizza();
+                PizzaComposite NYStyleClam = new PizzaComposite( "New York Clam");
+
+                NYStyleClam.add(new PizzaIngredient("Cheddar", 250, 2.50));
+                NYStyleClam.add (new PizzaIngredient("Clam", 100, 4.00));
+
+                return NYStyleClam;
+
+
             case "veggie":
-                return new NYStyleVeggiePizza();
+                PizzaComposite NYStyleVeggie = new PizzaComposite( "New York Veggie");
+
+                PizzaComposite mixedVeg = new PizzaComposite ( "Mixed Veg");
+
+                mixedVeg.add(new PizzaIngredient("Peppers", 40, 1.00));
+                mixedVeg.add(new PizzaIngredient("Mushroom", 60, 1.00));
+                mixedVeg.add(new PizzaIngredient("Onion", 55, 1.00));
+
+
+                NYStyleVeggie.add(new PizzaIngredient("Cheddar", 250, 2.50));
+                NYStyleVeggie.add (mixedVeg);
+                return NYStyleVeggie;
+
             default:
                 return null;
         }
     }
 }
 
- */
