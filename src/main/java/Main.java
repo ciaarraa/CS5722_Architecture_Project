09dd149655c2cfa22833d289609046299ef2c656
David O'Riordan <oriordandj@gmail.com>
2021-03-04 22:55:49 +0000

    NYPizzaStore test done in the same style as chicago

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,5 @@
 import com.Factory.ChicagoPizzaStore;
-//import com.Factory.NYPizzaStore;
+import com.Factory.NYPizzaStore;
 import com.Factory.PizzaStore;
 
 
@@ -8,15 +8,15 @@ public class Main {
 
     public static void main(String[] args){
 
-        //setting up the new york and chicago stores
-     // PizzaStore nyPizzaStore = new NYPizzaStore();
+      // setting up the new york and chicago stores
+      PizzaStore nyPizzaStore = new NYPizzaStore();
       PizzaStore chicagoPizzaStore = new ChicagoPizzaStore();
 
-      //ordering cheese pizza from ny store
-     // nyPizzaStore.orderPizza("cheese");
+      // ordering cheese pizza from ny store
+      nyPizzaStore.orderPizza("cheese");
 
-      //ordering pepperoni pizza from chicago store
-        chicagoPizzaStore.orderPizza("cheese");
+      // ordering pepperoni pizza from chicago store
+      chicagoPizzaStore.orderPizza("cheese");
 
 
 
