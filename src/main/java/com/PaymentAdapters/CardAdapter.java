166a705a86edcfeb8691ede0a7ba55373c56b154
David O'Riordan <oriordandj@gmail.com>
2021-04-08 15:45:46 +0100

    Added minimal comments to PaymentAdapters
    Added minimal comments to clarify some of the arbitrary decisions made for the sake of simplicity

--- src/main/java/com/PaymentAdapters/CardAdapter.java
+++ src/main/java/com/PaymentAdapters/CardAdapter.java
@@ -1,6 +1,11 @@
 package com.PaymentAdapters;
 import java.util.UUID;
-
+/*
+  Adapter Design Pattern
+  If for some reason we need to use the 'original/old' card payment interface
+  with a client that expects the Stripe interface, we ignore the authorisation token
+  and just process the amount via the card payment interface.
+*/
 public class CardAdapter implements StripePaymentInterface{
     CardPaymentInterface card;
     String AuthToken;
