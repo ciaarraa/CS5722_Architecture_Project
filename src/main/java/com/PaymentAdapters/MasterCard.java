3b10914ff8417081973c97e5b28e70fca7917f09
David O'Riordan <oriordandj@gmail.com>
2021-04-08 20:51:04 +0100

    Step1 - re organise payment adapters to have obvious state transitions
    So that we can hook in the interceptor. Mastercard DONE.

--- src/main/java/com/PaymentAdapters/MasterCard.java
+++ src/main/java/com/PaymentAdapters/MasterCard.java
@@ -1,22 +1,65 @@
 package com.PaymentAdapters;
-//import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeUnit;
 
 public class MasterCard implements CardPaymentInterface{
     @Override
     public boolean makePayment(double billFinalAmount) {
-        boolean paymentSuccess = false;
-        System.out.printf("Making a payment of %f with a card...\n", billFinalAmount);
-        //TimeUnit.SECONDS.sleep(5);
-        paymentSuccess = true;
-        return paymentSuccess;
+        boolean paymentInstructionSentSuccess = false;
+        boolean paymentConfirmationReceived = false;
 
+        paymentInstructionSentSuccess = sendPaymentInstructionToMastercardServer();
+        if (paymentInstructionSentSuccess){
+            System.out.printf("Sent request to charge %f to a card...\n", billFinalAmount);
+        }else{
+            System.out.printf("ERROR sending request to charge %f to card...\n", billFinalAmount);
+        }
+
+        paymentConfirmationReceived = receiveTransactionConfirmationFromMastercardServer();
+        if (paymentConfirmationReceived){
+            System.out.printf("SUCCESS - Transaction (charge %f to card) confirmed.\n", billFinalAmount);
+        }else{
+            System.out.printf("ERROR - Transaction (charge %f to card) NOT confirmed.\n", billFinalAmount);
+        }
+
+        return paymentConfirmationReceived;
     }
+
     @Override
     public boolean refundPayment(double refundFinalAmount) {
-        boolean paymentSuccess = false;
-        System.out.printf("Refunding a payment of %f to a card...\n", refundFinalAmount);
-        //TimeUnit.SECONDS.sleep(5);
-        paymentSuccess = true;
-        return paymentSuccess;
+        boolean paymentInstructionSentSuccess = false;
+        boolean paymentConfirmationReceived = false;
+
+        paymentInstructionSentSuccess = sendPaymentInstructionToMastercardServer();
+        if (paymentInstructionSentSuccess){
+            System.out.printf("Sent request to refund %f to a card...\n", refundFinalAmount);
+        }else{
+            System.out.printf("ERROR sending request to refund %f to card...\n", refundFinalAmount);
+        }
+
+        paymentConfirmationReceived = receiveTransactionConfirmationFromMastercardServer();
+        if (paymentConfirmationReceived){
+            System.out.printf("SUCCESS - Transaction (refund %f to card) confirmed.\n", refundFinalAmount);
+        }else{
+            System.out.printf("ERROR - Transaction (refund %f to card) NOT confirmed.\n", refundFinalAmount);
+        }
+
+        return paymentConfirmationReceived;
+    }
+
+    private boolean sendPaymentInstructionToMastercardServer() {
+        try {
+            TimeUnit.SECONDS.sleep(2);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return true;
+    }
+    private boolean receiveTransactionConfirmationFromMastercardServer() {
+        try {
+            TimeUnit.SECONDS.sleep(2);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return true;
     }
 }
